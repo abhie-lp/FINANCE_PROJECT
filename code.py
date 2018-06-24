05efa95cd68a01f8100286cc6f39edf9b2a189c5
abhie-lp <abhie.lp@gmail.com>
2018-06-24 22:46:37 +0530

    Added Comments and stored data as .csv

--- code.py
+++ code.py
@@ -15,14 +15,14 @@ def get_page_source(url):
 
 # Scraping the data for the desired company
 def scraping(company):
-    scrape_data = []											# To store the values of the stock data
+    scrape_data = []									# To store the values of the stock data
     # default page-url that will be used to get the data of different companies
     page_url = "https://markets.financialcontent.com/stocks/quote/historical?Symbol=%s&Month=6&Year=2018&Range=12"
-    source = get_page_source(page_url % company)				# To store the page-source of the companies data
-    soup = bs.BeautifulSoup(source, "lxml")						# converiting the page-source to BeautifulSoup object to start scraping
+    source = get_page_source(page_url % company)		# To store the page-source of the companies data
+    soup = bs.BeautifulSoup(source, "lxml")				# converiting the page-source to BeautifulSoup object to start scraping
     table_data = soup.find("table", class_="quote_detailed_price_table data").find_all("tr")	# all table data of that class and tr tag
     for row in table_data:
-        r_data = row.find_all("td")								# Storing all td tag
+        r_data = row.find_all("td")						# Storing all td tag
         scrape_data.append([data.text for data in r_data])
     return scrape_data
 
@@ -35,10 +35,10 @@ def to_csv(frame, name):
     frame.to_csv(f"{name}_data.csv", encoding="utf-8")
 
 
-companies = ("AAPL", "GOOGL", "MSFT")							# Name of the companies whose data will be scraped and analysed
+companies = ("AAPL", "GOOGL", "MSFT")					# Name of the companies whose data will be scraped and analysed
 
 
 for company in companies:
-    scrape_data = scraping(company)								# Function call to start the scraping of the page
-    frame = create_dframe(scrape_data[1:])						# Creating DataFrame from index 1 as 0 is blank
-    to_csv(frame, company)										# Storing the data as .csv to analyse it in future
+    scrape_data = scraping(company)						# Function call to start the scraping of the page
+    frame = create_dframe(scrape_data[1:])				# Creating DataFrame from index 1 as 0 is blank
+    to_csv(frame, company)								# Storing the data as .csv to analyse it in future
